eaab8480b1d14e08becbd49bd2fa09b35d552a61
Manan <aggarwalmanan18@gmail.com>
2025-05-08 21:33:33 +0530

    8/4

--- src/components/review.jsx
+++ src/components/review.jsx
@@ -9,7 +9,7 @@ const review = () => {
                 <div className="reviewbox centre text-lg font-semibold">Absolutelt amazing service!! It was so nice to have someone help me with essentials around the home. I even came home to flowers on the counter from them one week! They're so nice and really great to work with.</div>
                 <div className="reviewbox ">
                     <h1 className=" text-2xl font-bold">Create your acccount<br /> now!</h1>
-                    <button className='px-5 py-2 bg-orange-400 rounded-full m-5 font-bold'>Sign Up</button>
+                    <button className='px-5 py-2 bg-orange-400 rounded-full m-5 font-bold'>Sign Up Now</button>
                 </div>
                 <div className="reviewbox">
                     <h1 className=" text-2xl font-bold">Book your<br /> service!</h1>
