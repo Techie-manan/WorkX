a21062f78f9812a57d91baea6a27957d331b9ff3
Manan <aggarwalmanan18@gmail.com>
2025-05-11 22:54:44 +0530

    11-5

--- src/components/aboutW.jsx
+++ src/components/aboutW.jsx
@@ -4,7 +4,7 @@ import AniOnScroll from './AniOnScroll'
 
 const aboutW = () => {
     return (
-        <div className='relative h-screen bg-orange-500 p-20'>
+        <div className='relative h-screen bg-orange-400 p-20'>
             <div className='text'>
                   <div className='h-fit overflow-hidden'>
                   <AniOnScroll><h1 className='fadeY'>We are on a mission</h1> </AniOnScroll>
@@ -17,12 +17,12 @@ const aboutW = () => {
                  </div>
                 </div>
 
-            <div id="" className='w-2/3 h-fit sofa absolute left-1/2 top-1/5'>
-                <img src="src\assets\sofaa.png" alt="" /></div>
+            <div id="" className='w-2/3 h-[80vh] sofa absolute left-1/3 top-1/5 overflow-hidden'>
+                <img src="src\assets\sofaa.png" alt="" className='w-full h-full object-cover'/></div>
 
             <div className='para absolute top-3/5'>
                 <p>By living in a building powered by <br />WorkX, you have access to more<br /> than just to live</p>
-                <p className='mt-5'>Your apartment has provided you<br /> with your own delicatd home management  team fully integerated <br />into your building</p>
+                <p className='mt-5'>Your apartment has provided you with your own delicatd<br /> home management  team fully integerated <br />into your building</p>
             </div>
         </div>
     )
